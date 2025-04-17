bcd80b9315d6240b80ec5ea7c2f4b5ccec0ed07d
GeorGex3x383 <gjorgeemilio@gmail.com>
2025-04-17 11:22:37 -0600

    Subiendo introducción al curso

---
+++ 02_Introduction/main.py
@@ -0,0 +1,172 @@
+''' 
+Docstring 
+# ctrl + / command + /
+# goes in front of it
+# this program focuses on python basics
+# docstring is used for making multiline comments
+
+'''
+# print("helloworld!")
+# print('Hello this is my first python program')
+# print('This is a test')
+
+# course_title = "Python Programming Mastery"
+# title2 = "PPM"
+# advanced_course_2 = "Django course"
+# new_line = "I am a text on a new line"
+# # &Symbol = "Symbol"
+
+# # string manipulation
+# # string concatenation
+# complete_course = course_title + " " + title2 + "\n" + new_line
+# # seperator = course_title - title2
+# print(complete_course)
+# # print(seperator)
+
+# name = "Philip"
+# age = "19"
+# role = "Python engineer and instructor"
+
+# # f-string
+# # info = "My name is " + name + "I'm " + age + " yrs old" + "a " + role
+# info_with_f_string = f"My name is {name} I'm {age} yrs old a {role}"
+# # print(info)
+# print(info_with_f_string)
+
+# python primitive data types
+# string
+# integer
+# float
+# boolean 
+
+#python_inventor = "Guido van rossum"
+#integer = 19_000_000
+#type_of_integer = type(integer)
+#type_of_string = type(python_inventor)
+
+#bill = 90.99
+#type_of_bill = type(bill)
+#print(integer)
+
+#is_teenager = False
+# boolean = type(is_teenager)
+# print(boolean)
+
+'''
+type function in python is used in checking the type of an object
+integer is any natural number without floating points
+true false in javasvcript
+
+'''
+
+# python casting / datatype coversion / type casting
+
+#twitter_ceo = "Elon musk"
+#twitter_worth = "7_000_000_000"
+#age = 18
+#cost = 1945.77
+#is_rich = True
+#is_false = "100"
+
+# data_type = type(twitter_ceo)
+#is_rich_str = str(is_rich)
+#print(type(is_rich_str))
+
+#float_age = float(age)
+#print(float_age)
+# is_false_bool = bool(is_false)
+# print(is_false_bool)
+# conversion of string to int
+# int_musk = int(cost)
+# str_age = str(age)
+# str_cost = str(cost)
+
+# print(type(str_cost))
+# print(type(str_age))
+# print(int_musk)
+# programming_lng = input("What is your favorite programming language?")
+# print(type(programming_lng))
+# print(programming_lng)
+
+# age = input("How old are you? \n")
+# age_int = int(age)
+# print(type(age))
+# print(type(age_int))
+# print(age)
+
+name = input("what is your name? ")
+dream_company = input("What is the name of your dream company? ")
+programming_lng = input("what is your favorite programming language? ")
+
+description = f"I am {name}, I wish to work at {dream_company} and my favorite programming language is {programming_lng} "
+print(description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
