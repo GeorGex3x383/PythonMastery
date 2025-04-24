ad5d10867e509e2bf913b04a618ed76e240e72dd
GeorGex3x383 <gjorgeemilio@gmail.com>
2025-04-24 16:31:45 -0600

    Subiendo PythonFile IO

---
+++ 14_PythonFileIO/main.py
@@ -0,0 +1,55 @@
+import csv 
+# intro = open("intro.txt", "r")
+# print(intro.read()) 
+
+# intro.close()
+
+# with open("intro.txt", "r") as file :
+#     print(file.read())
+#     print(file.readline())
+#     print(file.readline())
+#     print(file.readline())
+
+# with open("/test.txt", "x") as test_file:
+#     pass
+
+
+# with open("intro.txt", "w") as new_file:
+#     new_file.write("I live in Ogun state, Nigeria")
+
+# with open("intro.txt", "a") as updated_file:
+#     updated_file.write("I'm philip\nA python engineer\nI'm 19 yrs old")
+
+data = [
+    ['Name', 'Age', 'City'],
+    ['John', 25, 'New York'],
+    ['Alice', 30, 'London'],
+    ['Bob', 35, 'Paris']
+]
+
+# writing into a csv file
+# with open("final code/data.csv", "w") as data_file:
+#     writer = csv.writer(data_file)
+#     writer.writerows(data)
+#     writer.writerow(['Johnny', 20, 'Florida'])
+
+# reading from a csv file
+with open("data.csv", "r") as data_file:
+    reader = csv.reader(data_file)
+    
+    for row in reader:
+        print(row)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
